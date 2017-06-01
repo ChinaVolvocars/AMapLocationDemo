b96be09f7c8f8a6c52be66286049d914aa23e065
Tiger <atlantisspeed@gmail.com>
2017-06-01 11:44:00 +0800

    高德定位Demo

---
+++ logcation/src/main/java/com/meahu/MainActivity.java
@@ -0,0 +1,116 @@
+package com.meahu;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends CheckPermissionsActivity implements View.OnClickListener {
+
+    private TextView location;
+    private AMapLocationClient mLocationClient;
+    private AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    aMapLocation.getLocationType();
+                    aMapLocation.getLatitude();
+                    aMapLocation.getAccuracy();
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(aMapLocation.getTime());
+                    df.format(date);
+                    String address = aMapLocation.getAddress();
+                    aMapLocation.getCountry();
+                    aMapLocation.getProvince();
+                    aMapLocation.getCity();
+                    aMapLocation.getDistrict();
+                    aMapLocation.getStreet();
+                    aMapLocation.getStreetNum();
+                    aMapLocation.getCityCode();
+                    aMapLocation.getAdCode();
+                    aMapLocation.getAoiName();
+                    location.setText(address);
+                } else {
+                    Log.e("AmapError", "location Error, ErrCode:" + aMapLocation.getErrorCode()
+                            + ", errInfo:" + aMapLocation.getErrorInfo());
+                }
+                Log.i("Tag", aMapLocation.getAddress());
+                mLocationClient.stopLocation();
+                mLocationClient.onDestroy();
+            }
+        }
+    };
+    private AMapLocationClientOption mLocationOption;
+    private Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        location = (TextView) findViewById(R.id.location);
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(this);
+        String s = sHA1(getApplicationContext());
+        System.out.println("============SHA1:" + s);
+
+    }
+
+
+    public static String sHA1(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result = hexString.toString();
+            return result.substring(0, result.length() - 1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        mLocationClient.setLocationListener(mLocationListener);
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setNeedAddress(true);
+        mLocationOption.setOnceLocation(false);
+        mLocationOption.setWifiScan(true);
+        mLocationOption.setMockEnable(false);
+        mLocationOption.setInterval(2000);
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationClient.startLocation();
+    }
+}
